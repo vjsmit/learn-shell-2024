67de25850485ac465eb87b5e0d3cb36e562fb51e
vjsmit <vjsmit97@gmail.com>
2024-02-16 18:09:15 +0530

    using catalogue as variable

--- 01-Variable.sh
+++ 01-Variable.sh
@@ -1,9 +1,9 @@
 a=10
-nocolor="\e[0m"
+no_color="\e[0m"
 
 echo Value of a is $adollar
 echo Value of a is ${a}-dollar
 
-echo color is -e "\e[33mRED COLOR${no_color}"
+echo -e "\e[33mRED COLOR${no_color}"
 
 echo -e "\e[31m  \e[0m"
